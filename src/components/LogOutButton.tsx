9d8225138ec2fb070ce57e4cc8a5de123a83764d
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-09-25 19:58:40 +0900

    ログアウト機能を実装

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -1,13 +1,28 @@
+import { router } from "expo-router"
+import { signOut } from "firebase/auth"
 import React from "react"
 import {
+  Alert,
   StyleSheet,
   Text,
   TouchableOpacity
 } from "react-native"
+import { auth } from "../config"
+
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => {
+        router.replace("/auth/log_in")
+      })
+      .catch((error) => {
+        console.log("error", error)
+        Alert.alert("ログアウトに失敗しました")
+      })
+}
 
 const LogOutButton = () => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>ログアウト</Text>
     </TouchableOpacity>
   )
